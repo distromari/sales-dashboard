47b3b495de25e3352d1061225c40b54e2136bd7b
distromari <mariheloisa010@gmail.com>
2021-11-02 17:21:52 -0300

    Project created

---
+++ backend/src/main/java/com/salesdashboard/dhsales/DhsalesApplication.java
@@ -0,0 +1,13 @@
+package com.salesdashboard.dhsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhsalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhsalesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/salesdashboard/dhsales/DhsalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesdashboard.dhsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhsalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
